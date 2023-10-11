1f786850a3d6d47817f0d72b5935f0bc2e086c6b
unknown <193305@iberopuebla.mx>
2023-10-11 13:26:00 -0600

    tailwind add

--- resources/views/forms/login.blade.php
+++ resources/views/forms/login.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.simple')
 
 @section('content')
-    <div style="width: 23rem;" class="mx-auto d-flex flex-column gap-4 my-5  p-3 border border-2 rounded rounded-lg">
-        <form class="d-flex flex-column gap-4" method="post" action="{{ route('login.submit') }}">
+    <div class="mx-auto flex flex-col gap-4 my-5 p-3 border-2 border-sky-800 rounded w-96">
+        <form class="flex flex-col gap-4" method="post" action="{{ route('login.submit') }}">
             @csrf
-            <h2>Inicio de Sesión</h2>
+            <h2 class="text-3xl">Inicio de Sesión</h2>
             <div>
-                <label for="email" class="form-label">Correo Electrónico</label>
+                <label for="email" class="text-lg">Correo Electrónico</label>
                 <input autocomplete="email" class="form-control" name="correo" type="email" placeholder="Ingrese un correo">
                 @if ($errors->has('correo'))
                     <span class="text-danger">{{ $errors->first('correo') }}</span>
@@ -14,7 +14,7 @@
             </div>
 
             <div>
-                <label for="password" class="form-label">Contraseña</label>
+                <label for="password" class="text-lg">Contraseña</label>
                 <input class="form-control" name="contraseña" type="password" placeholder="Ingrese su contraseña">
                 @if ($errors->has('contraseña'))
                     <span class="text-danger">{{ $errors->first('contraseña') }}</span>
@@ -24,10 +24,10 @@
             @if ($message)
                 <span class="text-danger">{{ $message }}</span>
             @endif
-            <button class="btn btn-primary" type="submit">Ingresar</button>
+            <button class="border-2  border-sky-800 rounded-lg p-2" type="submit">Ingresar</button>
 
         </form>
-        <a class="text-decoration-none" href={{ route('resetPassword.form') }}>
+        <a class="text-decoration-none text-sky-700" href={{ route('resetPassword.form') }}>
             ¿Se te olvido tu contraseña?
         </a>
 
