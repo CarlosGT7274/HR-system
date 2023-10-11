c2493eb734ce4117bcb8613d8500e296af5e54b1
unknown <193305@iberopuebla.mx>
2023-10-11 16:45:36 -0600

    Iconos de Fontawesome

--- resources/views/forms/resetPassword.blade.php
+++ resources/views/forms/resetPassword.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.simple')
 
 @section('content')
-    <div style="width: 23rem;" class="mx-auto d-flex flex-column gap-4 my-5 p-3 border border-2 rounded rounded-lg">
+    <div class="mx-auto flex flex-col gap-4 my-5 p-3 rounded w-96 h-96">
         <form class="d-flex flex-column gap-4" style="width: 20rem;" method="post" action="{{ route('login.submit') }}">
             @csrf
-            <h2>Reset Password</h2>
+            <h2 class="text-3xl">Cambiar Contraseña</h2>
             <div>
-                <label for="email" class="form-label">Correo Electrónico</label>
+                <label for="email" class="text-lg mb-2">Correo Electrónico</label>
                 <input autocomplete="email" class="form-control" name="correo" type="email"
                     placeholder="Ingrese un correo">
                 @if ($errors->has('correo'))
@@ -17,10 +17,9 @@
             @if ($message)
                 <span class="text-danger">{{ $message }}</span>
             @endif
-            <button class="btn btn-primary" type="submit">Enviar Correo</button>
+            <button class="border-2 border-sky-800 rounded-lg p-2 mx-auto" type="submit">Enviar Correo</button>
         </form>
-
-        <a class="text-decoration-none" href={{ route('login.form') }}>
+        <a class="text-decoration-none text-sky-700" href={{ route('login.form') }}>
             Regresar
         </a>
     </div>
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/js/app.js", "resoucers/css/app.css"],
+            input: [
+                "resources/js/app.js",
+                "resoucers/css/app.css",
+                "resources/icons/css/all.min.css",
+            ],
             refresh: true,
         }),
     ],
