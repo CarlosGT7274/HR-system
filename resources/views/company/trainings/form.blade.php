5a088f4768311732974f8cd3ab0645fffb0dedaa
unknown <193305@iberopuebla.mx>
2023-10-31 13:58:21 -0600

    update capacitaciones

--- resources/views/company/trainings/form.blade.php
+++ resources/views/company/trainings/form.blade.php
@@ -14,24 +14,23 @@
                 placeholder="Descripción de la capacitación" defaultValue="" />
         </div>
 
-        <div class="xl:col-start-2 xl:col-end-4">
-            <label for="poblacion">Población</label>
-            <x-input id="poblacion" icon="" autocomplete="" type="text" name="población"
-                placeholder="Población de la unidad" defaultValue="" />
-        </div>
-
         @if (!empty($empleados))
             <div class="xl:col-start-2 xl:col-end-4">
-                <label for="empleados">Empleados: </label>
-                <select id="empleados">
-                    @for ($i = 0; $i < count($empleados); $i++)
-                        <option value="{{ $i }}">
-                            {{ $empleados[$i]['nombre'] . ' ' . $empleados[$i]['apellidoP'] . ' ' . $empleados[$i]['apellidoM'] }}
-                        </option>
-                    @endfor
-                </select>
+                <label for="empleados">Empleados</label>
+                <div
+                    class="mb-1 border-b-2 border-b-ldark flex flex-row gap-2 items-center justify-around hover:border-b-primary p-2">
+                    <select class="" id="empleados">
+                        @for ($i = 0; $i < count($empleados); $i++)
+                            <option value="{{ $i }}">
+                                {{ $empleados[$i]['nombre'] . ' ' . $empleados[$i]['apellidoP'] . ' ' . $empleados[$i]['apellidoM'] }}
+                            </option>
+                        @endfor
+                    </select>
 
-                <button type="button" onclick="addUser()">Agregar empleado</button>
+                    <button
+                        class="border-2 text-primary border-primary rounded-lg p-2 hover:bg-primary hover:text-light font-semibold"
+                        type="button" onclick="addUser()">Agregar empleado</button>
+                </div>
             </div>
 
             <section class="xl:col-start-1 xl:col-end-5 grid lg:grid-cols-2 gap-16" id="empleados_container">
@@ -41,23 +40,66 @@
 
     <script>
         const empleados = @json($empleados);
+        let empleadosCap = [];
 
         function addUser() {
             let select = document.getElementById("empleados")
-            let opcionSeleccionada = select.value
+            let emp_i = select.value
 
-            let nuevoRecuadro = document.createElement("div")
-            nuevoRecuadro.className = "flex flex-col";
+            if (!empleadosCap.includes(emp_i)) {
+                const newEmployee = document.createElement("div")
+                newEmployee.className = "container";
 
-            nuevoRecuadro.innerHTML = empleados[opcionSeleccionada]['nombre']
+                const botonEliminar = document.createElement("button");
+                botonEliminar.type = 'button';
+                botonEliminar.name = emp_i;
+                botonEliminar.className = "p-2 border-r-2 h-10 border-ldark"
 
-            let inputDate = document.createElement("input")
-            inputDate.type = "date"
-            inputDate.className = "mt-2";
+                const icon = document.createElement("i")
+                icon.className = "fa-solid fa-trash text-danger"
 
-            nuevoRecuadro.appendChild(inputDate)
+                botonEliminar.appendChild(icon)
 
-            document.getElementById("empleados_container").appendChild(nuevoRecuadro)
+                botonEliminar.addEventListener("click", function() {
+                    const index = empleadosCap.indexOf(botonEliminar.name)
+                    empleadosCap.splice(index, 1);
+                    newEmployee.remove();
+                });
+
+                newEmployee.appendChild(botonEliminar);
+
+                const inputContainer = document.createElement("div");
+                inputContainer.className = "employee";
+
+                const label = document.createElement("label")
+                label.innerHTML = empleados[emp_i]['nombre'] + ' ' + empleados[emp_i]['apellidoP'] + ' ' + empleados[
+                    emp_i]['apellidoM']
+                label.className = "font-semibold"
+                label.for = `empleados[${empleadosCap.length}][fecha]`
+
+                const inputDate = document.createElement("input")
+                inputDate.id = `empleados[${empleadosCap.length}][fecha]`
+                inputDate.type = 'date';
+                inputDate.name = `empleados[${empleadosCap.length}][fecha]`;
+                inputDate.value = new Date().toISOString().split('T')[0];
+                inputDate.className = "cursor-pointer"
+
+                var id_employee = document.createElement("input");
+                id_employee.type = "hidden";
+                id_employee.name = `empleados[${empleadosCap.length}][id_empleado]`;
+                id_employee.value = empleados[emp_i]['id_empleado'];
+
+                inputContainer.appendChild(label)
+                inputContainer.appendChild(inputDate)
+                inputContainer.appendChild(id_employee)
+
+                newEmployee.appendChild(inputContainer)
+
+
+                document.getElementById("empleados_container").appendChild(newEmployee)
+
+                empleadosCap.push(emp_i);
+            }
         }
     </script>
 @endsection
