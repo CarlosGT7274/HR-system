85243fba6ba839923e20e37594fb9b7cd0d95302
unknown <193305@iberopuebla.mx>
2023-12-07 13:09:07 -0600

    Guia del programador

--- resources/views/general/unauthorized.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.simple')
-
-@section('content')
-    <div class="d-flex justify-content-center align-items-center" style="height: 100vh">
-        <h1 class="block text-center">Sin Autorizacion</h1>
-    </div>
-@endsection
